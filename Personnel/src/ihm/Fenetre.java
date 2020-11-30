f0163dc64ab012ae6695a91c8eac49367b8e4cd4
inees24 <72389212+inees24@users.noreply.github.com>
2020-11-30 14:43:27 +0100

    début d'interface graphique

--- Personnel/src/ihm/Fenetre.java
+++ Personnel/src/ihm/Fenetre.java
@@ -1,16 +1,23 @@
 package ihm;
 
 import javax.swing.*;
+import javax.swing.JPanel;
+import java.awt.Dimension;
+import java.awt.Color;
 
 public class Fenetre
 {
  public static void main(String[] args)
  {
   JFrame f = new JFrame();
+	JPanel panneau = new JPanel();
+	panneau.setPreferredSize(new Dimension(250, 150));
   f.setVisible(true);
   f.setTitle("Gestion des ligues");
   f.setExtendedState(f.getExtendedState() | JFrame.MAXIMIZED_BOTH);
-  
+  f.setContentPane(panneau);
+  panneau.setBackground(Color.pink);
+  f.setBackground(Color.pink);
   //f.setSize(200, 200);
   f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
  }
