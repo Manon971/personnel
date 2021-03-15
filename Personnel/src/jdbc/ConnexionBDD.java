2fa55d9cd42d8bbc04613b7bf7c8f2c958371556
Madery Romain <72389130+Exarilo@users.noreply.github.com>
2021-03-15 15:14:45 +0100

    Connexion BDD

---
+++ Personnel/src/jdbc/ConnexionBDD.java
@@ -0,0 +1,89 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import personnel.*;
+
+
+public class ConnexionBDD implements Passerelle  {
+	static Connection connection;
+
+	public static void main(String[] args) {
+
+		
+		System.out.println(CredentialsExample.getUrl());
+
+		try {
+			connection = DriverManager.getConnection(CredentialsExample.getUrl(), CredentialsExample.getUser(), CredentialsExample.getPassword());
+			System.out.println("La connexion a été effectué");
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("Echec de la connexion a la BDD");
+		}
+
+	}
+	
+	
+	public GestionPersonnel getGestionPersonnel() 
+	{
+		GestionPersonnel gestionPersonnel = new GestionPersonnel();
+		try 
+		{
+			String requete = "select * from ligue";
+			Statement instruction = connection.createStatement();
+			ResultSet ligues = instruction.executeQuery(requete);
+			while (ligues.next())
+				gestionPersonnel.addLigue(ligues.getInt(1), ligues.getString(2));
+		}
+		catch (SQLException e)
+		{
+			System.out.println(e);
+		}
+		return gestionPersonnel;
+	}
+
+	@Override
+	public void sauvegarderGestionPersonnel(GestionPersonnel gestionPersonnel) throws SauvegardeImpossible 
+	{
+		close();
+	}
+	
+	public void close() throws SauvegardeImpossible
+	{
+		try
+		{
+			if (connection != null)
+				connection.close();
+		}
+		catch (SQLException e)
+		{
+			throw new SauvegardeImpossible(e);
+		}
+	}
+	
+	@Override
+	public int insert(Ligue ligue) throws SauvegardeImpossible 
+	{
+		try 
+		{
+			PreparedStatement instruction;
+			instruction = connection.prepareStatement("insert into ligue (nom) values(?)", Statement.RETURN_GENERATED_KEYS);
+			instruction.setString(1, ligue.getNom());		
+			instruction.executeUpdate();
+			ResultSet id = instruction.getGeneratedKeys();
+			id.next();
+			return id.getInt(1);
+		} 
+		catch (SQLException exception) 
+		{
+			exception.printStackTrace();
+			throw new SauvegardeImpossible(exception);
+		}		
+	}
+
+}
