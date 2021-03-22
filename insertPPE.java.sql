204e77fb7fff3b17ccad0dcf62914ad0796fb860
inees24 <72389212+inees24@users.noreply.github.com>
2021-03-22 14:21:12 +0100

    BASE DE DONNÉES

--- insertPPE.java.sql
+++ insertPPE.java.sql
@@ -2,7 +2,7 @@ insert into LIGUE values (1, 'ligue de football');
 insert into LIGUE values (2, 'ligue de basketball');
 insert into LIGUE values (3, 'ligue de baseball');
 
-insert into EMPLOYE values (1, 'nom1', 'prenom1', 'nom1.prenom1@gmail.com', 'password1', '24-10-2010', '24-10-2012', 0, 1, 1);
+insert into EMPLOYE values (1, 'nom1', 'prenom1', 'nom1.prenom1@gmail.com', 'password1', NULL, NULL, 0, 1, 1);
 insert into EMPLOYE values (2, 'nom2', 'prenom2', 'nom2.prenom2@gmail.com', 'password2', NULL, NULL, 1, 0, 1);
 insert into EMPLOYE values (3, 'nom3', 'prenom3', 'nom3.prenom3@gmail.com', 'password3', NULL, NULL, 0, 0, 1);
 insert into EMPLOYE values (4, 'nom4', 'prenom4', 'nom4.prenom4@gmail.com', 'password4', NULL, NULL, 0, 0, 2);
